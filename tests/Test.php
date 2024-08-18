cf4c38b9cccd45cf4be2d779484179d139299b62
Melqui Vunge <melquilegend@gmail.com>
2024-08-18 10:59:45 -0300

    Add tests directory and example test

---
+++ tests/Test.php
@@ -0,0 +1,12 @@
+// tests/ExampleTest.php
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}

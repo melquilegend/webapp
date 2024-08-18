746e851541a230999de43d20839d9dd6c300a436
Melqui Vunge <melquilegend@gmail.com>
2024-08-18 11:02:51 -0300

    Add @covers annotation to ExampleTest

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @covers \App\SomeClass::someMethod
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
